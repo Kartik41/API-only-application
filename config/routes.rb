1b52b31a8883168b95c6a2cd6491ca817ec66031
Kartik41 <kartik.chauhan@unthinkable.co>
2021-04-12 17:12:29 +0530

    API only application created

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {registrations: 'registrations'}
+  namespace :api do
+    namespace :v1 do
+      get 'post/index'
+      get 'posts/index'
+      post 'posts/create'
+      #resources :posts
+      post :auth, to: 'authentication#create'
+      get '/auth' => 'authentication#fetch'
+    end
+  end
+  #root 'posts#index'
+ # resources :posts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
